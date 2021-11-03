1cabb58fad80db540c9c1c13d2d7dba6cde0f798
Frithjof Thorvik <frithjot@stud.ntnu.no>
2021-11-03 18:59:09 +0100

    Added loading animation on page load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { UserProvider } from "@auth0/nextjs-auth0";
+import Router from "next/router";
 
 import { IAppProps } from "../types/pageInterfaces";
 import Layout from "../components/Layout";
+import Loader from "../components/Loader";
 
 import "../styles/globals.scss";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 /**
  * The main component for entire application
@@ -13,10 +16,22 @@ import "../styles/globals.scss";
  * @return  {JSX.Element}           - The JSX code for displaying entire application
  */
 const App: FC<IAppProps> = ({ Component, pageProps }): JSX.Element => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  // Activate loading when route is changed
+  Router.events.on("routeChangeStart", () => {
+    setIsLoading(true);
+  });
+
+  // Deactivate loader when route change is completed
+  Router.events.on("routeChangeComplete", () => {
+    setIsLoading(false);
+  });
+
   return (
     <UserProvider>
       <Layout>
-        <Component {...pageProps} />
+        {isLoading ? <Loader loader="Circles" /> : <Component {...pageProps} />}
       </Layout>
     </UserProvider>
   );
